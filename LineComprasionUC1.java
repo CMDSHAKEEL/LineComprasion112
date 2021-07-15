570bf26e50538ccabd57e2b008616625d248eb73
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-07-15 23:17:28 +0530

    WELCOME_TO_LINE_COMPRASION

---
+++ LineComprasionUC1.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class LineComprasionUC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" WELCOME_TO_SNAKEAND LADDER");
+
+	}
+
+}
